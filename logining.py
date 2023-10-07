bb6cef865cacae8695fd7010f1b5b09be7e74e6f
BruSeLY <103708209+BruSeLY@users.noreply.github.com>
2023-10-07 09:49:06 +0900

    front

---
+++ logining.py
@@ -0,0 +1,45 @@
+import kivy
+import jwt
+import uuid
+import datetime
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from databaseconnect import *
+import pymysql
+try:
+    connection = pymysql.connect(host='37.140.192.80',
+                                 user='u0823922_codolo1',
+                                 password='codologia1',
+                                 cursorclass=pymysql.cursors.DictCursor,
+                                 database="u0823922_test")
+    print("successfully...")
+except Exception as ex:
+    print(ex)
+Builder.load_file('login.kv')
+
+
+class Logining(Widget):
+    def enter(self):
+        # try:
+        #     with cursor.connect as cursor:
+        #         pass
+        # except Exception as ex:
+        #     print(ex)
+        pass
+
+
+class RegApp(App):
+    def build(self):
+        return Logining()
+
+
+if __name__ == '__main__':
+    RegApp().run()
