b7ff0dd3df68cd755b7d8b8abedf063da3baa3b5
BruSeLY <103708209+BruSeLY@users.noreply.github.com>
2023-10-08 21:17:33 +0900

    database

--- point_choice.py
+++ point_choice.py
@@ -22,14 +22,35 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.screen import MDScreen
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
-
+import pymysql
+try:
+    connection = pymysql.connect(host='37.140.192.80',
+                                 user='u0823922_hakaton',
+                                 password='tB4nG4fN9sqG1vJ9',
+                                 cursorclass=pymysql.cursors.DictCursor,
+                                 database="u0823922_hakaton")
+    print("successfully...")
+except Exception as ex:
+    print(ex)
 Builder.load_file('point_choice.kv')
+
+
 class PointChoiceApp(Screen):
+
     def __init__(self, **kwargs):
         super(PointChoiceApp, self).__init__(**kwargs)
         self.point_choice = DropDown()
-        for index in range(10):
-            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
+        adress = []
+        try:
+            with connection.cursor() as cursor:
+                find_query = f"SELECT * FROM places"
+                cursor.execute(find_query)
+                adress = cursor.fetchall()
+                print(adress)
+        except Exception as ex:
+            print(ex)
+        for index in range(len(adress)):
+            btn = Button(text='Value %d' % adress[index]["Adress"], size_hint_y=None, height=44)
             btn.bind(on_release=lambda btn: self.point_choice.select(btn.text))
             self.point_choice.add_widget(btn)
         self.point_button.bind(on_release=self.point_choice.open)
